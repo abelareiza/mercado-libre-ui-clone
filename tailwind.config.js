f0b48f8038bd847bc436c7179c20ee6e2046f8d6
Abel <abelareiza@gmail.com>
2021-07-02 23:21:42 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        amarillo: '#fff159',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
